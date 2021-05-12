7bc9cc3f683c8eb33b3c77f1add3e9de9f87f038
SpringFlash <vadik2001.8928@gmail.com>
2021-05-12 15:27:08 +0300

    Добавлен vue-router

--- src/App.vue
+++ src/App.vue
@@ -2,48 +2,66 @@
   <div id="app">
     <h1>СДЕЛАЙТЕ ЗАКАЗ НАПРЯМУЮ ИЗ РЕСТОРАНА</h1>
     <div class="container">
-        <div class="interface">
-            <Menu />
-            <Cart />
-        </div>
-        <div id="product-list">
-          <ProductCard v-for="card of cardList" :key = "card.name"
-          :info="card" :market="data.markets[card.market]" />
-        </div>
+      <div class="interface">
+        <Menu />
+        <Cart />
+      </div>
+      <div id="product-list">
+        <ProductCard
+          v-for="card of cardList"
+          :key="card.name"
+          :info="card"
+          :market="data.markets[card.market]"
+        />
+      </div>
     </div>
     <Popup />
   </div>
 </template>
 
 <script>
-import ProductCard from '@/components/card.vue';
-import Menu from '@/components/menu.vue';
-import Cart from '@/components/cart.vue';
-import Popup from '@/components/popup.vue'
+import ProductCard from "@/components/card.vue";
+import Menu from "@/components/menu.vue";
+import Cart from "@/components/cart.vue";
+import Popup from "@/components/popup.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
-      hasData: false
-    }
+      hasData: false,
+    };
   },
   components: {
     ProductCard,
     Menu,
     Cart,
-    Popup
+    Popup,
   },
   computed: {
-    data() { return this.$store.getters.getData },
-    category() { return this.$store.getters.getCategory },
-    cards() { return this.$store.getters.getCards },
-    cardList() { return this.hasData ? this.cards.filter(card => card.category == this.category) : [] }
+    data() {
+      return this.$store.getters.getData;
+    },
+    category() {
+      return this.$route.params.category;
+    },
+    cards() {
+      return this.$store.getters.getCards;
+    },
+    cardList() {
+      return this.hasData
+        ? this.cards.filter((card) => card.category == this.category)
+        : [];
+    },
   },
   async mounted() {
-    this.$store.dispatch('getJson').then(() => this.hasData = true)
-  }
-}
+    this.$store.dispatch("getJson").then(() => {
+      this.hasData = true;
+      if (this.$route.path === "/")
+        this.$router.push(`/${Object.keys(this.data.categories)[0]}`);
+    });
+  },
+};
 </script>
 
 <style src="@/main.css"></style>
